695f72724613118e82eb8c022c2199eeba1e4b77
Sahithi Bhashyam <sahithibhasyam@gmail.com>
2022-11-06 17:08:43 +0530

    Create SubsetSum_Sorted.java

---
+++ LAB 11/lab11/src/SubsetSum_Sorted.java
@@ -0,0 +1,63 @@
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+
+public class SubsetSum_Sorted {
+  public static void display(int size, int[] elem, int[] x) {
+    for (int i = 0; i < size; i++) {
+      if (x[i] != 0) {
+        System.out.print(elem[i] + " ");
+      }
+    }
+    System.out.println();
+  }
+  public static void findElements(int size, int[] elem, int[] x,
+                                  int index, int currsum, int tgtsum) {
+    if (currsum == tgtsum) {
+      display(size, elem, x);
+      x[index] = 0;
+      return;
+    }
+    if ((size == 0) && (tgtsum != 0)) {
+      return;
+    }
+    if (currsum < tgtsum) {
+      x[index + 1] = 1;
+      findElements(size, elem, x, index+1, currsum + elem[index+1], tgtsum);
+    }
+    if (x[index] != 0) {
+      x[index] = 0;
+      currsum = currsum - elem[index];
+      if (index != size -1) {
+        if (currsum < tgtsum) {
+          x[index + 1] = 1;
+          findElements(size, elem, x, index, currsum, tgtsum);
+        }
+      }
+    }
+  }
+
+  public static void main(String[] args) throws FileNotFoundException {
+    String filename = args[0];
+    File inFile = new File(filename);
+    Scanner in = new Scanner(inFile);
+    int n = in.nextInt();
+    int sum = in.nextInt();
+    int[] elem = new int[n];
+    int[] x = new int[n];
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+      elem[i] = in.nextInt();
+      total += elem[i];
+    }
+    Arrays.sort(elem);
+    if (total < sum) {
+      System.out.println("No solution possible");
+      return;
+    }
+    x[0] = 1;
+    findElements(n, elem, x, 0, elem[0], sum);
+
+  }
+}
+
